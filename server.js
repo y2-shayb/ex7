3ee1fce4075747f66e4d341e5b1c38a2fad6acab
y2-shayb <shayb@yad2.co.il>
2020-02-25 15:05:39 +0200

    finished

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+app.use(express.json());
+const port = 1337;
+const recepiesUrl = 'https://recipes-goodness.herokuapp.com/recipes/';
+global.counter = 0;
+
+const middleware = (req, res, next) => {
+    if (counter >= 5) {
+        res.status(400).send({error: "Too many requests"});
+        res.end();
+    } else {
+        next();
+    }
+};
+
+app.use(middleware);
+
+app.get('/recipe/:ingredient', async (req,res) => {
+    counter++;
+    const ingredient = req.params.ingredient;
+    try {
+        const { data } = await axios.get(`${recepiesUrl}${ingredient}`);
+
+        if (!data.results || !data.results.length) {
+            res.send({ empty: true });
+        }
+
+        const topThree = [];
+        let count = 0;
+
+        while (count < 3 && data.results[count]) {
+            count++;
+            topThree.push(data.results[count]);
+        }
+
+        res.send(topThree);
+    } catch (e) {
+        res.status(500).res.send('error occured');
+    }
+});
+
+
+app.listen(port, () => {
+    console.log('ex7 is running!! ! ! ');
+});
